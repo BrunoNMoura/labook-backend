d11ada0a200586b672e3b2047ac4ff0a14bb3859
BrunoNMoura <bnmalemao@gmail.com>
2023-08-20 11:05:20 -0300

    :sparkles:service folder creation

---
+++ src/services/tokenManager.ts
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { USER_ROLES } from "../models/User";
+
+
+export interface TokenPayload {
+    id:string,
+    name:string;
+    role:USER_ROLES;
+}
+
+export class TokenManager {
+    public createToken = (payload:TokenPayload) =>{
+        const token = jwt.sign(payload, process.env.JWT_KEY as string, {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+        })
+
+        return token
+    }
+
+    public getPauload = (token:string): TokenPayload | null =>{
+        try {
+            const payload = jwt.verify(token,process.env.JWT_KEY as string)
+
+            return payload as TokenPayload
+        } catch (error) {
+            return null
+        }
+    }
+}
