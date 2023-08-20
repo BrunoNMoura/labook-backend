649c8d8e24c402bbe68ecb3d329b999e7af6bb50
BrunoNMoura <bnmalemao@gmail.com>
2023-08-20 11:46:35 -0300

    :sparkles:id and token implementation

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,6 +1,11 @@
 
 import { Request, Response } from "express"
 import { UserBusiness } from "../business/UserBusiness"
+import { LoginSchema } from "../dtos/users/login.dto"
+import { ZodError } from "zod"
+import { BaseError } from "../errors/BaseError"
+import { GetUsersSchema } from "../dtos/users/getUser.dto"
+import { SignupSchema } from "../dtos/users/signup.dto"
 export class UserController {
 
   constructor(private userBusiness: UserBusiness) { }
@@ -8,10 +13,9 @@ export class UserController {
   public getUsers = async (req: Request, res: Response): Promise<void> => {
     try {
 
-      const input = {
+      const input = GetUsersSchema.parse({
         q: req.query.q,
-        token: req.headers.authorization
-      }
+      });
 
       const output = await this.userBusiness.getUsers(input)
 
@@ -32,11 +36,11 @@ export class UserController {
   public singUp = async (req: Request, res: Response): Promise<void> => {
 
     try {
-      const input ={
+      const input = SignupSchema.parse({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
-      }
+        password: req.body.password,
+      });
 
       const output = await this.userBusiness.singUp(input);
 
@@ -53,4 +57,26 @@ export class UserController {
           }
     }
   }  
+
+  public login = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const input = LoginSchema.parse({
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      const output = await this.userBusiness.login(input);
+
+      res.status(200).send(output);
+    } catch (error) {
+      console.log(error);
+      if (error instanceof ZodError) {
+        res.status(400).send(error.issues);
+      } else if (error instanceof BaseError) {
+        res.status(error.statusCode).send(error.message);
+      } else {
+        res.status(500).send("Erro inesperado");
+      }
+    }
+  };
 }
